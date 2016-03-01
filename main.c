7e73789080256dd5f31c5a95e43c5aa90ce83c3d
schummelbiene <schummelbiene@web.de>
2016-03-01 14:42:05 +0100

    working example with less bugs

--- main.c
+++ main.c
@@ -5,17 +5,6 @@
 #include "string.h"
 
 
-//Typen:
-// ganzzahlig:
-/* int i = -5; */
-/* unsigned int ui = 5; */
-/* long l = -123523952244; */
-/* unsigned long ul = 0x32AB; //hexadezimal */
-/* // gebrochenzahlig */
-/* float f = 0.5f; */
-/* double d = 0.424345345d; */
-//mehr in stdint.h, z.B. uint8_t (unsigned 8bit-Typ)
-
 typedef struct {
 	int number_of_nodes; //number of nodes
 	int deg; //degree
@@ -24,42 +13,122 @@ typedef struct {
 
 typedef struct {
 	d_graph d_graph; //
-  float* X; // cartesian coordinates of every node n
-  float* Y; // cartesian coordinates of every node n
+  double* X; // cartesian coordinates of every node n
+  double* Y; // cartesian coordinates of every node n
 } c_graph;
 
 
-float pi = 3.1415; 
-float cool(int i, int n);
-//float abs(float x);
-float min(float x, float y);
+double pi = 3.1415; 
+double cool(int i, int n);
+double min(double x, double y);
 d_graph d_minigraph();
+c_graph set_nodes(d_graph g, int k);
 c_graph c_minigraph();
 void output(c_graph g );
 void graphit(int a, int b);
 void move(c_graph g, int n);
-float dist(float x1, float x2, float y1, float y2);
+double dist(double x1, double x2, double y1, double y2);
 
 int main() {
   
 	c_graph g;
 	g = c_minigraph();
-	//	write(*,'I') g%n;
-	printf("%i, %i \n", g.d_graph.number_of_nodes, g.d_graph.deg);
-	for (int i = 0; i < g.d_graph.number_of_nodes; i++){
-		int degree = g.d_graph.deg;
-		printf("%i, %i, %i, ", g.d_graph.A[degree*i], g.d_graph.A[degree*i+1], g.d_graph.A[degree*i+2]);
-		printf("%f, %f, \n", g.X[i], g.Y[i]); 
-	}
 
-	for (int i = 1; i<2000; i++){; 
-				move(g, i);
+	for (int i = 1; i<100; i++){; 
+						move(g, i);
 	}
 	output(g);
 	return 1;
 }
 
-// tetrahedron :)
+// single move routine ---------------------------------------------------------------
+
+void move(c_graph g, int n){
+	int degree = g.d_graph.deg;
+	int node_length = g.d_graph.number_of_nodes;
+	double constant;
+	constant = pow(node_length/pi, 0.5);
+	
+	double* force_x;  // force_x[i] returns the force in x-direction for node i, see algorithm.pdf
+	double* force_y;  // force_y[i] returns the force in y-direction for node i, see algorithm.pdf
+	
+	force_x = malloc(node_length*sizeof(double));
+	force_y = malloc(node_length*sizeof(double));
+	
+	// Step I:
+	memset(force_x, 0, node_length * sizeof(double));
+	memset(force_y, 0, node_length * sizeof(double)); 
+
+	// Step II:
+	for (int i = 0; i < node_length; i++){
+		for (int j = 0; j < degree; j++){
+			
+			double distance = dist(g.X[i], g.Y[i], g.X[g.d_graph.A[degree*i+j]], g.Y[g.d_graph.A[degree*i+j]]);
+			printf("%i, %i, %f , %f, %f, loop, distance: %f \n", i, j, constant, force_x[i], force_y[i], distance);
+			force_x[i] -= constant * (g.X[i]- g.X[g.d_graph.A[degree*i+j]]) * distance * distance;
+			force_y[i] -= constant * (g.Y[i]- g.Y[g.d_graph.A[degree*i+j]]) * distance * distance;
+		}
+	}
+
+	// Step III:
+
+	double c = cool(n, node_length);
+	
+	for (int i = 3; i < node_length; i++){
+
+		double norm = dist(0, 0, force_x[i], force_y[i]);
+		
+		g.X[i] +=  min(norm, c) * force_x[i]/norm; 
+		g.Y[i] +=  min(norm, c) * force_y[i]/norm; 
+		printf("Iteration %i \n", i);
+	}
+
+	return;
+}
+
+// initializing ---------------------------------------------------------
+// k: number of nodes on the unit circle
+c_graph set_nodes(d_graph g, int k){
+	c_graph h;
+	h.X = malloc(g.number_of_nodes*sizeof(double));
+	h.Y = malloc(g.number_of_nodes*sizeof(double));	
+
+	for (int i = 0; i < k; i++ ){
+		h.X[i] = cos(2*pi*i/k);
+		h.Y[i] = sin(2*pi*i/k);
+	}
+	for (int i = k; i < g.number_of_nodes; i++){
+		h.X[i] = 0;
+		h.Y[i] = 0;
+
+	}
+	h.d_graph = g;
+	return h;
+}
+
+// utilities ------------------------------------------------------------
+
+double min(double x, double y){
+	if (x >= y) {
+		return y;
+	} else {
+		return x;
+	}
+}
+
+double dist(double x1, double x2, double y1, double y2){
+	double d;
+	d = pow((x1 - y1)*(x1 - y1) + (x2 - y2)*(x2 - y2),0.5);
+	return d;
+}
+
+double cool(int i, int n){
+	double result;
+	result = pow(pi/n, 0.5)/(1 + pi/n*pow(i, 1.5));
+	return result;
+}
+
+// Example ---------------------------------------------------------------------------
 
 d_graph d_minigraph(){ 
 	d_graph g;
@@ -87,127 +156,46 @@ d_graph d_minigraph(){
 	g.A[12] = 1;
 	g.A[13] = 3;
 	g.A[14] = 5;
-	g.A[15] = 2;
-	g.A[16] = 3;
-	g.A[17] = 5;
+	g.A[15] = 3;
+	g.A[16] = 4;
+	g.A[17] = 2;
 	return g;
 }
 
 c_graph c_minigraph(){
 	c_graph tetrahedron;
 	tetrahedron.d_graph = d_minigraph();
-	tetrahedron.X = malloc(tetrahedron.d_graph.number_of_nodes*sizeof(float));
-	tetrahedron.Y = malloc(tetrahedron.d_graph.number_of_nodes*sizeof(float));
+	tetrahedron.X = malloc(tetrahedron.d_graph.number_of_nodes*sizeof(double));
+	tetrahedron.Y = malloc(tetrahedron.d_graph.number_of_nodes*sizeof(double));
 
 	/* for (int i = 0; i < tetrahedron.d_graph.number_of_nodes; i++) {	 */
-	/* 	tetrahedron.X[i] = ((float)i)/((float)tetrahedron.d_graph.number_of_nodes ); */
-	/* 	tetrahedron.Y[i] = (float)(i+1)/(float)(tetrahedron.d_graph.number_of_nodes*i+1); */
+	/* 	tetrahedron.X[i] = ((double)i)/((double)tetrahedron.d_graph.number_of_nodes ); */
+	/* 	tetrahedron.Y[i] = (double)(i+1)/(double)(tetrahedron.d_graph.number_of_nodes*i+1); */
 	/* } */
 	
 	tetrahedron.X[0] = -0.5;
-	tetrahedron.Y[0] = -0.866;
+	tetrahedron.Y[0] = -0.5*sqrt(3.0);
 	tetrahedron.X[1] = 1;
 	tetrahedron.Y[1] = 0;
 	tetrahedron.X[2] = -0.5;
-	tetrahedron.Y[2] = 0.866;
+	tetrahedron.Y[2] = 0.5*sqrt(3.0);
 
-	float eps;
-	eps = 0.1;
+	double eps;
+	eps = -0.1;
 	
 	tetrahedron.X[3] = -0.5*eps;
-	tetrahedron.Y[3] = -0.866*eps;
-	tetrahedron.X[4] =  2*eps;
+	tetrahedron.Y[3] = -0.5*sqrt(3.0)*eps;
+	tetrahedron.X[4] = 1*eps;
 	tetrahedron.Y[4] = 0*eps;
 	tetrahedron.X[5] = -0.5*eps;
-	tetrahedron.Y[5] = 0.866*eps;
-	
+	tetrahedron.Y[5] = 0.5*sqrt(3.0)*eps;
+
+	//tetrahedron = set_nodes(tetrahedron.d_graph, 3);
 	return tetrahedron;
 }
 
-float min(float x, float y){
-	if (x >= y) {
-		return y;
-	} else {
-		return x;
-	}
-}
 
-float dist(float x1, float x2, float y1, float y2){
-	float d;
-	d = powf((x1 - y1)*(x1 - y1) + (x2 - y2)*(x2 - y2),0.5);
-	return d;
-}
-
-float cool(int i, int n){
-	float result;
-	result = powf(n/pi, 0.5)/(1 + pi/n*powf(i, 1.5));
-	return result;
-}
-
-// n: iteration cycle
-void move(c_graph g, int n){
-	int degree = g.d_graph.deg;
-	int node_length = g.d_graph.number_of_nodes;
-	float constant;
-	constant = node_length/pi;
-	
-	float* force_x;  // force_x[i] returns the force in x-direction for node i, see algorithm.pdf
-	float* force_y;  // force_y[i] returns the force in y-direction for node i, see algorithm.pdf
-	
-	force_x = malloc(node_length*sizeof(float));
-	force_y = malloc(node_length*sizeof(float));
-	
-	// Step I:
-	memset(force_x, 0, node_length * sizeof(float));
-	memset(force_y, 0, node_length * sizeof(float));
-
-	// Step II:
-	
-	for (int i = 0; i < node_length; i++){
-		
-		for (int j = 0; j < degree; j++){
-			
-
-			printf("%i, %i, %f , %f, %f, loop \n", i, j, constant, force_x[i], force_y[i]);
-			
-			force_x[i] = force_x[i] - constant *
-				(g.X[i]- g.X[g.d_graph.A[3*i+j]]) *
-				(g.X[i]- g.X[g.d_graph.A[3*i+j]]) *
-				(g.X[i]- g.X[g.d_graph.A[3*i+j]]);
-
-			force_y[i] = force_y[i] - constant *
-				(g.Y[i]- g.Y[g.d_graph.A[3*i+j]]) *
-				(g.Y[i]- g.Y[g.d_graph.A[3*i+j]]) *
-				(g.Y[i]- g.Y[g.d_graph.A[3*i+j]]);
-
-		}
-	}
-
-	// Step III:
-
-	
-	
-	for (int i = 3; i < node_length; i++){
-		
-		printf("%f,  %f  before\n", g.X[i],g.Y[i]);
-
-		float norm = dist(0, 0, force_x[i], force_y[i]);
-		float c    = cool(n, node_length);
-		
-		g.X[i] = g.X[i] + 0.1*min(norm, c) * force_x[i]/norm; 
-		g.Y[i] = g.Y[i] + 0.1*min(norm, c) * force_y[i]/norm; 
-
-		printf("%f,  %f after \n", g.X[i],g.Y[i]);
-	}
-
-	return;
-}
-
-void graphit(int a, int b) {
-  printf("Huhu: %i, %i", a, a + b);
-
-  return;
-}
+// Output routine ------------------------------------------------------------------
 
 void output(c_graph g ){
 	FILE *f = fopen("output_graph.tex", "w");
